44c298fcc8414ef7456d9eff73b25a6d3124970b
Iain Mullan <iain.mullan@gmail.com>
2015-02-10 11:34:29 +0000

    prod db config

--- deploy/app/config/local/database.php
+++ deploy/app/config/local/database.php
@@ -42,6 +42,6 @@ return array(
 			'schema'   => 'public',
 		),
 
-	),
+	)
 
 );
--- deploy/bootstrap/start.php
+++ deploy/bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('gabriela*'),
+    'local' => array('gabriela*'),
+    'production' => array('brick7'),
 
 ));
 
