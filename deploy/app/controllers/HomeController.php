537716b23b3ce1eda190a4b9046d0faab350d5fb
Iain Mullan <iain.mullan@gmail.com>
2015-02-10 12:06:24 +0000

    check for empty artist search

--- deploy/app/controllers/HomeController.php
+++ deploy/app/controllers/HomeController.php
@@ -8,25 +8,22 @@ class HomeController extends BaseController {
 
 		$artistName = Input::get('artist', false);
 
-		if (!$artistName) {
-			return View::make('home');
+		if ($artistName) {
+			/*
+			GET https://api.spotify.com/v1/search?type=artist&q={$artistName}
+			*/
+			$results = $api->search($artistName, 'artist');
+
+			$artists = $results->artists->items;
+
+			if (count($artists) > 1) {
+				return View::make('artists.select', ['artists' => $artists]);
+			} else if (count($artists) > 0) {
+				return Redirect::to('/artist/'.$artists[0]->id);
+			}
 		}
 
-		/*
-		GET https://api.spotify.com/v1/search?type=artist&q={$artistName}
-		*/
-		$results = $api->search($artistName, 'artist');
-
-		$artists = $results->artists->items;
-
-		if (count($artists) > 1) {
-
-//			return Response::json($artists);
-
-			return View::make('artists.select', ['artists' => $artists]);
-		}
-
-		return Redirect::to('/artist/'.$artists[0]->id);
+		return View::make('home');
 	}
 
 }
