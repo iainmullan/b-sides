0b996e0196bfd565881a34ca5237a0b073c4d2f3
Iain Mullan <iain.mullan@gmail.com>
2015-06-11 11:37:47 -0400

    display more artists on home page

--- deploy/app/controllers/HomeController.php
+++ deploy/app/controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends BaseController {
 			}
 		}
 
-		$popular = Artist::orderBy('playlist_count', 'DESC')->limit(5)->get();
+		$popular = Artist::orderBy('playlist_count', 'DESC')->limit(20)->get();
 
 		return View::make('home', [
 			'popular' => $popular
